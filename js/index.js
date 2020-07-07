681460b2526409bb9f730a9ee7bd55169fd83bdf
Galo Sandoval <galosan92@gmail.com>
2020-07-07 14:01:37 -0700

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+const navLinks = document.querySelectorAll('.nav-link');
+const docH2s = document.querySelectorAll('h2');
+const docPs = document.querySelectorAll('p');
+
+const header = document.querySelector('.intro')
+let funBus = header.querySelectorAll('img');
+
+const imgContent = document.querySelectorAll('.img-content')
+
+const bottomImg = document.querySelector('.content-destination')
+const actualImg = bottomImg.querySelector('img')
+
+const buttons = document.querySelectorAll('.btn')
+// console.log(imgContent)
+
+logo.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'orange'
+  setTimeout(function() {
+    event.target.style.color = 'black';
+  }, 500);
+}, false)
+
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', event => {
+      buttons[i].innerHTML = `Signed up: ${event.detail}`;
+    });
+}
+
+imgContent.
+
+mouseTarget.addEventListener('mouseenter', e => {
+    mouseTarget.style.border = '5px dotted orange';
+    enterEventCount++;
+    addListItem('This is mouseenter event ' + enterEventCount + '.');
+  });
+  
+
